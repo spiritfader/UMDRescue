18e363db31b55572c366e7103fec5ea5cb9ba3fc
spiritfader <31056954+spiritfader@users.noreply.github.com>
2024-02-19 20:59:17 -0500

    fix oe_malloc.h

--- oe_malloc.h
+++ oe_malloc.h
@@ -1,1 +1,12 @@
-404: Not Found
+#ifndef __OE_MALLOC_H__
+
+#define __OE_MALLOC_H_
+
+int  mallocinit();
+#define malloc oe_malloc
+#define free  oe_free
+void *oe_malloc(size_t size);
+void oe_free(void *p);
+int  mallocterminate();
+
+#endif
