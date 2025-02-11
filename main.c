08bf1060cfda761258ea5ce4cb075f00d7c4e78e
spiritfader <31056954+spiritfader@users.noreply.github.com>
2025-02-11 18:20:31 -0500

    add GPLv3 source header

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/*
+ UMDRescue -- create archive quality images of UMD optical discs
+ 
+ Copyright (C) 2024, 2025 spiritfader
+ Copyright (C) 2024, 2025 krazynez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <pspsdk.h>
 #include <pspctrl.h>
 #include <pspdebug.h>
